dc5d722f861939b307205acbc14fc9c95ea33971
bitic1hao <57070198+bitic1hao@users.noreply.github.com>
2026-01-02 22:24:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const NEZHA_KEY = process.env.NEZHA_KEY || '';              // 哪吒v1的NZ_CLI
 const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'my-claw.xcr002.de5.net';          // 固定隧道域名,留空即启用临时隧道
 const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiYzI3ZWY4MGMwYzFiZmZmMGU3ZDM3NjUyZThhODY0YjUiLCJ0IjoiMjA2ZDkzOTItZDMxMy00Y2NiLTllODMtNDlhMDZmYjUyM2MxIiwicyI6Ik5EQTVOVEF4TnpndE5UVTFNeTAwTVRJMExUaGhOell0TXpBeU9XTXpNVGN5TTJJMSJ9';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
 const ARGO_PORT = process.env.ARGO_PORT || 8001;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
-const CFIP = process.env.CFIP || 'www.visa.com';        // 节点优选域名或优选ip  
+const CFIP = process.env.CFIP || 'xcr.cf.cname.vvhan.com';        // 节点优选域名或优选ip  
 const CFPORT = process.env.CFPORT || 443;                   // 节点优选域名或优选ip对应的端口
 const NAME = process.env.NAME || 'my-claw';                        // 节点名称
 
